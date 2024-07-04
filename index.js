6cbb015790556b9d3dc0840f970096db10d7e90c
sportyfred <ivanboring@gmail.com>
2024-07-04 13:46:10 +0200

    re

--- index.js
+++ index.js
@@ -29,6 +29,15 @@ const port = process.env.PORT || 3000;
 
 var result = []
 const tra = []
+const { FileClient } = require('@vonage/server-client');
+const fileClient = new FileClient({
+     apiKey: process.env.API_KEY,
+  apiSecret: process.env.API_SECRET,
+  applicationId: process.env.APP_ID,
+  privateKey: process.env.PRIVATE_KEY,
+});
+
+// Save the file to a specific location
 
 
 
@@ -43,19 +52,12 @@ const textDirectory = (__dirname + '/public/transcriptions')
         // TODO
     };
 
-const Nexmo = require('nexmo');
-const nexmo = new Nexmo({
-  apiKey: process.env.API_KEY,
-  apiSecret: process.env.API_SECRET,
-  applicationId: process.env.APP_ID,
-  privateKey: process.env.PRIVATE_KEY
-});
-
 
 
 app.use(express.static('public'));
 app.use(express.static('uploads'));
 app.use(bodyParser.json());
+
 
 
 
@@ -67,8 +69,10 @@ app.get('/recordings', (req, res) => {
     // check that the files are audio files
     const fileNameArr = file.split('.');
     return fileNameArr[fileNameArr.length - 1] === 'webm';
-  }).map((file) => `/${file}`);
+  }).map((file) => `${file}`);
+  io.emit('filename', files);
   return res.json({ success: true, files });
+  
 });
 
 
@@ -121,114 +125,49 @@ app.get('/answer', (req, res) => {
 // events from Nexmo app
 app.post('/event', (req, res) => {
   res.status(204);
+  console.log(req.body);
 
   
 });
 
 // defined in `/answer`, called when recording completed
 app.post('/voicemail', (req, res) => {
- 
-  let filename = uniqueName.uniqueNamesGenerator() + '.webm';
-  let path = __dirname + '/uploads/' + filename;
-  consolg.log(path);
- upload.single(req.body.recording_url, 'recording.webm');
-  nexmo.files.save(req.body.recording_url, path, (err, response) => {
-    if (err) {
-      res.status(500);
-      return console.error(err);
-    }
-    return console.log('success write file')
-  });
+       console.log('start')
 
-    io.emit('voicemail', filename);
+let path1 = path.join(__dirname, "/uploads/");
+let date = Date.now();
+let filename = path1 + date + '.webm';
+console.log(filename); 
+fileClient.downloadFile(
+  req.body.recording_url, filename);
+console.log('ues record');
 
+     let files = fs.readdirSync(path.join(__dirname, 'uploads'));
+  files = files.filter((file) => {
+    // check that the files are audio files
+    const fileNameArr = file.split('.');
+    return fileNameArr[fileNameArr.length - 1] === 'webm';
+  }).map((file) => `${file}`);
+     io.emit('filename', files);
+console.log(files);
 });
 
 
 
-var walk = function (dir, action, done) {
-
-    // this flag will indicate if an error occured (in this case we don't want to go on walking the tree)
-    var dead = false;
-
-    // this flag will store the number of pending async operations
-    var pending = 0;
-
-    var fail = function (err) {
-        if (!dead) {
-            dead = true;
-            done(err);
-        }
-    };
-
-    var checkSuccess = function () {
-        if (!dead && pending == 0) {
-            done();
-        }
-    };
-
-    var performAction = function (file, stat) {
-        if (!dead) {
-            try {
-                action(file, stat);
-            }
-            catch (error) {
-                fail(error);
-            }
-        }
-    };
-
-    // this function will recursively explore one directory in the context defined by the variables above
-    var dive = function (dir) {
-        pending++; // async operation starting after this line
-        fs.readdir(dir, function (err, list) {
-            if (!dead) { // if we are already dead, we don't do anything
-                if (err) {
-                    fail(err); // if an error occured, let's fail
-                }
-                else { // iterate over the files
-                    list.forEach(function (file) {
-                        if (!dead) { // if we are already dead, we don't do anything
-                            var path = dir + "/" + file;
-                            pending++; // async operation starting after this line
-                            fs.stat(path, function (err, stat) {
-                                if (!dead) { // if we are already dead, we don't do anything
-                                    if (err) {
-                                        fail(err); // if an error occured, let's fail
-                                    }
-                                    else {
-                                        if (stat && stat.isDirectory()) {
-                                            dive(path); // it's a directory, let's explore recursively
-                                        }
-                                        else {
-                                            performAction(path, stat); // it's not a directory, just perform the action
-                                        }
-                                        pending--;
-                                        checkSuccess(); // async operation complete
-                                    }
-                                }
-                            });
-                        }
-                    });
-                    pending--;
-                    checkSuccess(); // async operation complete
-                }
-            }
-        });
-    };
-
-    // start exploration
-    dive(dir);
-};
 
 io
     .on('connection', function (socket) {
-        walk(rootDirectory, function (path, stat) {
-            
-            let text = path;
-const split = text.split("uploads/");
+         let files = fs.readdirSync(path.join(__dirname, 'uploads'));
+  files = files.filter((file) => {
+    // check that the files are audio files
+    const fileNameArr = file.split('.');
+    return fileNameArr[fileNameArr.length - 1] === 'webm';
+  }).map((file) => `${file}`);
 
-            socket.emit('filename', split[1]);
+
+console.log(files)
+
+            socket.emit('filename', files);
         }, function (err) {
             if (err) {
                 socket.emit('error', 'Something went wrong');
@@ -237,32 +176,9 @@ const split = text.split("uploads/");
             }
         });
 
-             walk(textDirectory, function (path, stat) {
             
-            let text = path;
-
-
-const split = text.split("transcriptions/");
-const meningar = []
-
-var array = JSON.parse(fs.readFileSync('./public/transcriptions/'+split[1], 'utf8'));
-
-for (i in array['channels'][0]['transcript']){
-meningar[i] = (array['channels'][0]['transcript'][i]['raw_sentence'])
-
-
-        }
-   
-    socket.emit('textfilename',meningar)
-}, function (err) {
-            if (err) {
-                socket.emit('error', 'Something went wrong');
-            } else {
-                socket.emit('done');
-            }
-        });
 
        
-    });
+  
 
 http.listen(port);
