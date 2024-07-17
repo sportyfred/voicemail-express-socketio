78451e8c0e1d9e9b22089ebd256a3f6b46cc6d6c
sportyfred <ivanboring@gmail.com>
2024-07-17 14:01:55 +0200

    fd

--- index.js
+++ index.js
@@ -88,7 +88,7 @@ app.use(bodyParser.json());
 
 
 app.post('/record', upload.single('audio'), (req, res) => res.json({ success: true }));
-
+app.post('/cron', (req,res) => res.json({cronjob: true}));
 app.get('/recordings', (req, res) => {
   let files = fs.readdirSync(path.join(__dirname, 'uploads'));
   files = files.filter((file) => {
